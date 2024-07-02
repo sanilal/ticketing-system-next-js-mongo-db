ba1179643c114d69b9b1ef9c22f0edde35d05eb7
Sanilal P Sathyan <sanilal@gmail.com>
2024-07-02 19:33:00 +0400

    completed

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,21 +1,23 @@
-import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Link from "next/link"
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 const Nav = () => {
-  return <nav>
-    <div>
+  return (
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-4">
         <Link href="/">
-            <FontAwesomeIcon icon={faHome} className="icon" />
+          <FontAwesomeIcon icon={faHome} className="icon" />
         </Link>
         <Link href="/TicketPage/new">
-            <FontAwesomeIcon icon={faTicket} className="icon" />
+          <FontAwesomeIcon icon={faTicket} className="icon" />
         </Link>
-    </div>
-    <div>
-        <p classname="text-default-text">user@example.com</p>
-    </div>
-  </nav>
-}
+      </div>
+      <div>
+        <p className=" text-default-text">user@example.com</p>
+      </div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
